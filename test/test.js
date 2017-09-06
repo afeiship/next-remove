b7181c091a0b792edec1f5914c7ea39181fe7a92
afeiship <1290657123@qq.com>
2017-09-06 10:48:58 +0800

    issue: intial

---
+++ test/test.js
@@ -0,0 +1,66 @@
+var assert = require('assert');
+var nx = require('next-js-core2');
+require('../src/next-remove');
+
+describe('next-remove', function () {
+
+  it('remove pure items', function () {
+    var ary1 = [1,2,3,4,5,6];
+    var ary2 = ['a','b','c','d','e'];
+
+    assert.equal(nx.remove(ary1,[1]).join(''),'23456');
+    assert.equal(nx.remove(ary2,['d','e']).join(''),'abc');
+  });
+
+
+  it('remove object items', function () {
+    var ary1 = [
+      {
+        key:1,
+        value:'value1'
+      },
+      {
+        key:2,
+        value:'value2'
+      },
+      {
+        key:3,
+        value:'value3'
+      },
+      {
+        key:4,
+        value:'value4'
+      }
+    ];
+
+    var ary2 = [
+      {
+        key:1,
+        value:'value1'
+      },
+      {
+        key:2,
+        value:'value2'
+      },
+      {
+        key:3,
+        value:'value3'
+      },
+      {
+        key:4,
+        value:'value4'
+      }
+    ];
+
+    var result1 = nx.remove( ary1,[ ary1[0],ary1[1] ] );
+    var result2 = nx.remove( ary2,[ ary2[0] ] );
+
+    assert.equal( (result1.map(item=>item.key)).join(''),'34' );
+    assert.equal( (result2.map(item=>item.key)).join(''),'234' );
+  });
+
+
+
+
+
+});
